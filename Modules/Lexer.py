57785cc4d7d6ff734a9245ecee4153d16811b044
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-04-28 23:27:41 +0900

    Update Lexer.py

--- Modules/Lexer.py
+++ Modules/Lexer.py
@@ -1,11 +1,9 @@
 from Grammer import Token
 
 
-def getToken(string: str) -> int:
-    if string not in Token.token.keys():
-        return Token.token["unknown"]
-    return Token.token[string]
-
-
 class Lexer:
-    ...
+    @staticmethod
+    def getToken(string: str) -> int:
+        if string not in Token.token.keys():
+            return Token.token["unknown"]
+        return Token.token[string]
