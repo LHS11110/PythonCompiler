17937618c4d032bc2192e1b4dddf63ac0f4bd13f
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-04-30 00:47:39 +0900

    Update Token.py

--- Modules/Grammer/Token.py
+++ Modules/Grammer/Token.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
+
 def Enumerater(*str_list: str) -> dict[str, int]:
     return {str_list[idx]: idx for idx in range(len(str_list))}
 
 
-token: dict[str, int] = Enumerater(
+token: Optional[dict[str, int]] = Enumerater(
     *[
         n
         for n, _ in [
