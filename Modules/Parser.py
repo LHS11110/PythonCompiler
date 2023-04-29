f54217172bb290ef572120a7a219a8fe08835e54
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-04-30 03:05:11 +0900

    add state

--- Modules/Parser.py
+++ Modules/Parser.py
@@ -8,3 +8,8 @@ class state:
     IfState: bool = False
     IndentState: int = 0
     ExprState: bool = False
+
+    ClassSet: set[str] = set()
+    FuncSet: set[str] = set()
+    VarSet: set[str] = set()
+    StructSet: set[str] = set()
