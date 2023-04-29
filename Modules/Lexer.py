de9e19293626a24fdca4d520f36f7e1f5adc180a
Weird <ii3289857@gmail.com>
2023-04-30 01:14:33 +0900

    1

--- Modules/Lexer.py
+++ Modules/Lexer.py
@@ -44,6 +44,8 @@ class Lexer:
         pass
 
     def tokenize(self, input_text: str) -> list[tuple[str, str]]:
+        print(self.Pattern().patterns)
+
         tokens: list[tuple[str, str]] = []
         pos: int = 0
         length: int = len(input_text)
@@ -54,10 +56,10 @@ class Lexer:
                     tokens.append((token_type, match.group()))
                     pos += len(match.group())
                     break
-            else:
-                raise ValueError(f"Unexpected character at position {pos}")
+                else:
+                    raise ValueError(f"Unexpected character at position {pos}")
         return tokens
 
 
 if __name__ == "__main__":
-    print(Lexer().tokenize("f > g"))
+    print(Lexer().tokenize("asdf"))
