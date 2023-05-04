3a3ae224bc647319c43f774a5a080fbafa4281f0
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-05 04:37:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ input_txt = """for ((123, "Hello, World!"), ()), (add(), print), ({ar}), [hi()] 
 print(
     Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt))[
         Expression.Expression.getEnum(
-            Parser.Parser.cleanup(Lexer.Lexer().tokenize(input_text=input_txt)),
+            Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt)),
             1,
         )[1] :
     ],
