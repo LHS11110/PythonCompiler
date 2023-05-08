17f3f1e6f472b5b7645efcb411763c96b3dcde44
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-08 22:21:19 +0900

    Update Expression.py

--- Modules/Grammer/Expression.py
+++ Modules/Grammer/Expression.py
@@ -3,7 +3,45 @@ from Modules.Grammer.Checker import Checker
 from Modules.Grammer.Object import Object
 from Modules.Grammer.Container import Container
 
-priority: dict[str, int] = {}
+
+# 연산자 우선순위
+priority: dict[str, int] = {
+    "Object": 1,
+    "Container": 1,
+    "Indexing": 2,
+    "Slicing": 2,
+    "Call": 2,
+    "MemberAccess": 2,
+    "DMULTIPLY": 4,
+    "UNARY_MINUS": 5,
+    "UNARY_PLUS": 5,
+    "UNARY_NOT": 5,
+    "MULTIPLY": 6,
+    "DIVIDE": 6,
+    "FLOORDIV": 6,
+    "MODULO": 6,
+    "PLUS": 7,
+    "MINUS": 7,
+    "RBITSHIFT": 8,
+    "LBITSHIFT": 8,
+    "BITAND": 9,
+    "BITXOR": 10,
+    "BITOR": 11,
+    "IN": 12,
+    "NOTIN": 12,
+    "IS": 12,
+    "ISNOT": 12,
+    "LESS": 12,
+    "GREATER": 12,
+    "LEQUAL": 12,
+    "GEQUAL": 12,
+    "NEQUAL": 12,
+    "DEQUAL": 12,
+    "NOT": 13,
+    "AND": 14,
+    "OR": 15,
+    "TERNARY_OP": 16,
+}
 
 
 class Expression:
@@ -31,22 +69,25 @@ class Expression:
         tree["Type"] = "Indexing"
         tree["Elements"] = []
         count: int = 0
-        while count != 0 and count % 2 == 0 and codes[idx][0] != "RSQB":
+        while codes[idx][0] != "RSQB":
             if codes[idx][0] == "EOL" or codes[idx][0] == "INDENT":
                 idx += 1
 
             elif not count % 2:
-                if not count % 2 and codes[idx][0] == "COLON":  # 슬라이싱에서 객체가 생략됬다면
+                if codes[idx][0] == "COLON":  # 슬라이싱에서 객체가 생략됬다면
                     tree["Elements"].append({"Type": "Empty"})
                 else:
                     element, idx = Checker.codeMatch(
                         codes=codes,
                         idx=idx,
                         match_list=[
-                            Expression.getExpr,  # Expression은 계산식과 객체, 함수 호출, 인덱싱, 리터럴을 모두 포함한다.
+                            Object.getVar,
+                            Object.getLiteral,
+                            Expression.getExpr,
                             Container.getTuple,
                             Container.getSet,
                             Container.getList,
+                            Container.getDict,
                         ],
                     )
                     tree["Elements"].append(element)
@@ -58,6 +99,8 @@ class Expression:
             else:
                 idx += 1
                 count += 1
+        if not count % 2:  # 마지막 객체가 생략됬다면
+            tree["Elements"].append({"Type": "Empty"})
         if len(tree["Elements"]) == 0 or len(tree["Elements"]) > 3:
             raise SyntaxError()
         if len(tree["Elements"]) > 1:
@@ -65,6 +108,23 @@ class Expression:
         return (tree, idx + 1)
 
     @staticmethod
+    def getMemberAccess(
+        codes: list[tuple[str, str]], idx: int
+    ) -> tuple[dict[str, Any], int]:
+        if codes[idx][0] != "DOT":
+            raise SyntaxError()
+        idx += 1
+        tree: dict[str, Any] = {}
+        tree["Type"] = "MemberAccess"
+        tree["Member"], idx = Checker.codeMatch(
+            codes=codes, idx=idx, match_list=[Object.getVar]
+        )
+
+        return (tree, idx)
+
+    @staticmethod
     def getExpr(codes: list[tuple[str, str]], idx: int) -> tuple[dict[str, Any], int]:
         stack: list[str] = []
-        raise SyntaxError()
+        tree: dict[str, Any] = {}
+
+        return (tree, idx)
