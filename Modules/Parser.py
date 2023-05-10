e8513586e7b3822b02e232e36b7952906dc2661f
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-11 02:47:32 +0900

    Update Parser.py

--- Modules/Parser.py
+++ Modules/Parser.py
@@ -18,6 +18,7 @@ class state:
     StaticVarSet: set[str] = field(default_factory=set)
 
     FuncStack: list[str] = field(default_factory=list)
+    BracketStack: list[str] = field(default_factory=list)  #
 
 
 class Parser:
@@ -30,12 +31,24 @@ class Parser:
         _state: state = state()
         _state.EndOfLineState = True
         for code in codes:
-            if code[0] == "INDENT" and _state.EndOfLineState:
+            if (
+                code[0] == "INDENT"
+                and _state.EndOfLineState
+                and len(_state.BracketStack) == 0
+            ):
                 result.append(code)
-            elif code[0] == "EOL":
+            elif code[0] == "EOL" and len(_state.BracketStack) == 0:
                 result.append(code)
                 _state.EndOfLineState = True
-            elif code[0] != "INDENT":
+            elif code[0] in ["LSQB", "LBRACE", "LPAREN"]:
+                result.append(code)
+                _state.BracketStack.append(code[0])
+            elif code[0] in ["RSQB", "RBRACE", "RPAREN"]:
+                if _state.BracketStack[-1] != ("L" + code[0][1:]):
+                    raise SyntaxError()
+                result.append(code)
+                _state.BracketStack.pop()
+            elif code[0] != "INDENT" and code[0] != "EOL":
                 result.append(code)
                 _state.EndOfLineState = False
         return result
