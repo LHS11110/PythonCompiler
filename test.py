1f8c378defb25f5b24ea80389eb936955955e7ac
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-10 21:34:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,11 @@ from Modules.Grammer import Expression
 from Modules.Grammer import Container
 from Modules.Grammer import Object
 
-input_txt: str = '{123:213, "asd": 123}\n'
-
-print(Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt)), end="\n\n")
+input_txt: str = "[123::] \n"
 
 print(
     Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt))[
-        Container.Container.getDict(
+        Expression.Expression.getAnotherOp(
             Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt)),
             0,
         )[1] :
@@ -18,11 +16,8 @@ print(
 )
 
 print(
-    Container.Container.getDict(
+    Expression.Expression.getAnotherOp(
         Parser.Parser.cleanup(Lexer.Lexer.tokenize(input_text=input_txt)),
         0,
     )
 )
-
-
-print(4 << 2)
