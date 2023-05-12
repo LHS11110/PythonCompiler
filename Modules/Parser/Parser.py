3dffb2999e48c80dd9bcdc2bf4824f9aa0d135b0
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-12 23:40:19 +0900

    Update Parser.py

--- Modules/Parser/Parser.py
+++ Modules/Parser/Parser.py
@@ -21,38 +21,33 @@ class state:
     BracketStack: list[str] = field(default_factory=list)  #
 
 
-class Parser:
-    def __init__(self) -> None:
-        pass
+def cleanup(codes: list[tuple[str, str]]) -> list[tuple[str, str]]:
+    result: list[tuple[str, str]] = []
+    _state: state = state()
+    _state.EndOfLineState = True
+    for code in codes:
+        if (
+            code[0] == "INDENT"
+            and _state.EndOfLineState
+            and len(_state.BracketStack) == 0
+        ):
+            result.append(code)
+        elif code[0] == "EOL" and len(_state.BracketStack) == 0:
+            result.append(code)
+            _state.EndOfLineState = True
+        elif code[0] in ["LSQB", "LBRACE", "LPAREN"]:
+            result.append(code)
+            _state.BracketStack.append(code[0])
+        elif code[0] in ["RSQB", "RBRACE", "RPAREN"]:
+            if _state.BracketStack[-1] != ("L" + code[0][1:]):
+                raise SyntaxError()
+            result.append(code)
+            _state.BracketStack.pop()
+        elif code[0] != "INDENT" and code[0] != "EOL":
+            result.append(code)
+            _state.EndOfLineState = False
+    return result
 
-    @staticmethod
-    def cleanup(codes: list[tuple[str, str]]) -> list[tuple[str, str]]:
-        result: list[tuple[str, str]] = []
-        _state: state = state()
-        _state.EndOfLineState = True
-        for code in codes:
-            if (
-                code[0] == "INDENT"
-                and _state.EndOfLineState
-                and len(_state.BracketStack) == 0
-            ):
-                result.append(code)
-            elif code[0] == "EOL" and len(_state.BracketStack) == 0:
-                result.append(code)
-                _state.EndOfLineState = True
-            elif code[0] in ["LSQB", "LBRACE", "LPAREN"]:
-                result.append(code)
-                _state.BracketStack.append(code[0])
-            elif code[0] in ["RSQB", "RBRACE", "RPAREN"]:
-                if _state.BracketStack[-1] != ("L" + code[0][1:]):
-                    raise SyntaxError()
-                result.append(code)
-                _state.BracketStack.pop()
-            elif code[0] != "INDENT" and code[0] != "EOL":
-                result.append(code)
-                _state.EndOfLineState = False
-        return result
 
-    @staticmethod
-    def isEndOfLine(codes: list[tuple[str, str]]) -> bool:
-        return codes[-1][0] == "EOL"
+def isEndOfLine(codes: list[tuple[str, str]]) -> bool:
+    return codes[-1][0] == "EOL"
