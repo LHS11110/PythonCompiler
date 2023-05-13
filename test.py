cbb5d0b0fa8d4dd89ce5ce8a2f137aeb24ffe285
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-14 04:34:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from Modules.Parser import Object
 from Modules.Parser import Expression
 from Modules.Parser.Expression import priority
 
-input_txt: str = "- not - - 123 - - not 321\n"
+input_txt: str = "- not -asd.a[123]\n"
 
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
@@ -20,3 +20,16 @@ print(
     ),
     end="\n\n",
 )
+
+print(
+    Parser.cleanup(Lexer.tokenize(input_text=input_txt))[
+        Expression.getExpr(
+            Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
+            0,
+        )[1] :
+    ],
+    end="\n\n",
+)
+
+b = 10
+a = 10 if 20 else b := 2 + 1
