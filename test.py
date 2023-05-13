01c608846da738809c8309011596493ef9e6753d
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-14 01:01:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ from Modules.Lexer import Lexer
 from Modules.Parser import Parser
 from Modules.Parser import Container
 from Modules.Parser import Object
+from Modules.Parser import Expression
 from Modules.Parser.Expression import priority
 
-input_txt: str = '123           :           "asd"'
+input_txt: str = "- not - - 123 - - not 321\n"
 
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
@@ -13,12 +14,9 @@ print(
 )
 
 print(
-    Object.getKeyAndValue(
+    Expression.getExpr(
         Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
         0,
     ),
     end="\n\n",
 )
-
-
-print(priority)
