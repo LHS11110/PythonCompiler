dbde66a236af7c3a68c5fd84c26b357c7d1215b0
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-14 22:25:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from Modules.Parser import Object
 from Modules.Parser import Expression
 from Modules.Parser.Expression import priority
 
-input_txt: str = "- not -asd.a[123]\n"
+input_txt: str = "- not -asd.a[::] + 2 if True else 123\n"
 
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
@@ -30,6 +30,3 @@ print(
     ],
     end="\n\n",
 )
-
-b = 10
-a = 10 if 20 else b := 2 + 1
