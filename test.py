09eeaa024fa7d030a006d76f56c67cea3deeee8c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-18 11:36:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from Modules.Parser import Object
 from Modules.Parser import Expression
 from Modules.Parser.Expression import priority
 
-input_txt: str = "- not -asd.a[::] + 2 if True else 123\n"
+input_txt: str = "- not -asd.a[::]() ** (4 + 2) if True else 123\n"
 
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
