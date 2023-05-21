c2bac1f420124bf115ac1c71b4663b728306ff42
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-05-22 00:36:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ from Modules.Parser import Parser
 from Modules.Parser import Container
 from Modules.Parser import Object
 from Modules.Parser import Expression
+from Modules.Parser import Checker
 from Modules.Parser.Expression import priority
 
-input_txt: str = "i for i in [1, 2, 3]\n"
+input_txt: str = '{1: 2, "123": 123, {123}: [123]}\n'
+
 print(input_txt)
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
@@ -14,18 +16,22 @@ print(
 )
 
 print(
-    Object.getGenerator(
+    Container.getDict(
         Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
         0,
+        container_list=[Container.getList, Container.getSet],
+        obj_list=[Object.getLiteral, Object.getVar],
     ),
     end="\n\n",
 )
 
 print(
     Parser.cleanup(Lexer.tokenize(input_text=input_txt))[
-        Object.getGenerator(
+        Container.getDict(
             Parser.cleanup(Lexer.tokenize(input_text=input_txt)),
             0,
+            container_list=[Container.getList, Container.getSet],
+            obj_list=[Object.getLiteral, Object.getVar],
         )[1] :
     ],
     end="\n\n",
