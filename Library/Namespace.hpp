4a3c57e8e09af6e5a079f18e4b884548c8f7955c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-04 22:32:29 +0900

    Update Namespace.hpp

--- Library/Namespace.hpp
+++ Library/Namespace.hpp
@@ -1,4 +1,27 @@
 #ifndef _NAMESPACE_HPP
 #define _NAMESPACE_HPP
+#define LBITSHIFT(X) ((X << 1) + 1)
+#define RBITSHIFT(X) ((unsigned long long)X >> 1)
+#define Modulo(X, Y) (X & Y) // X % Y (Y == 2^n - 1)
 #include <vector>
+using namespace std;
+typedef unsigned long long ull;
+
+template <typename Type>
+class Namespace
+{
+private:
+    struct node
+    {
+        ull key;
+        Type
+    };
+
+    vector<node *> table;
+
+public:
+    Namespace();
+    auto operator[](const ull) -> Type &;
+};
+
 #endif
