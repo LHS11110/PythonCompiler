bca3039f6b4789e3375c64aeeab89c73b6db90d6
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-10 02:31:58 +0900

    Update Namespace.cpp

--- Library/Namespace.cpp
+++ Library/Namespace.cpp
@@ -1,53 +1,24 @@
 #include "Namespace.hpp"
 
-inline auto pyc::id() -> ull
-{
-    static ull _id = 0;
-    return _id++;
-}
-
-template <typename Type>
-pyc::Namespace<Type>::node::node(ull _k, const Type &_v)
-    : key(_k), value(_v)
-{
-}
-
 template <typename Type>
 pyc::Namespace<Type>::Namespace()
-    : table(vector<Namespace::node *>())
+    :
 {
 }
 
 template <typename Type>
-auto pyc::Namespace<Type>::operator[](const ull key) -> Type &
+auto pyc::Namespace<Type>::hash(const char *key) -> unsigned int
 {
-    ull idx, empty_idx;
-    idx = empty_idx = table.size();
-    if (idx)
+    int c = 0;
+    unsigned int hash = 0;
+    while (c = *key++)
     {
-        ull _mcache = 0;
-        node *ptr = nullptr;
-        goto find;
-        do
-        {
-            idx >>= 1;
-        find:
-            if ((ptr = table[_mcache = Modulo(key, idx)]) == nullptr)
-            {
-                empty_idx = _mcache;
-                continue;
-            }
-            if (ptr->key == key)
-                return ptr->value;
-        } while (idx);
+        hash += c;
+        hash += hash << 10;
+        hash ^= hash >> 6;
     }
-    if (empty_idx == table.size())
-    {
-        if (!empty_idx)
-            table.resize(1);
-        else
-            table.resize(empty_idx <<= 1);
-        return (table[Modulo(key, empty_idx)] = new node(key, Type()))->value;
-    }
-    return (table[empty_idx] = new node(key, Type()))->value;
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
 }
