dee76aef5fa8b5f3674d9f631db107be0f1ebd12
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-12 19:27:16 +0900

    Create main_test.cpp

---
+++ main_test.cpp
@@ -0,0 +1,11 @@
+#include "Namespace.hpp"
+#include <iostream>
+using namespace std;
+
+auto main(void) -> int
+{
+    cout << sizeof(pyc::Namespace<short>::bucket::space) << '\n';
+    cout << (8 * 32) << '\n';
+
+    return 0;
+}
