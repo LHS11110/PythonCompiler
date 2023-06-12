295a763ff16732b2bdc2c2a6cb75bb0e2cbb409c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-12 19:27:22 +0900

    Update Namespace.hpp

--- Library/Namespace.hpp
+++ Library/Namespace.hpp
@@ -1,7 +1,7 @@
 #ifndef _NAMESPACE_HPP
 #define _NAMESPACE_HPP
 #define Modulo(X, Y) (X & (Y - 1)) // X % Y, (Y == 2^n)
-#include <vector>
+#include <cstdlib>
 using namespace std;
 typedef unsigned long long ull;
 
@@ -10,11 +10,21 @@ namespace pyc
     template <typename Type>
     class Namespace
     {
+    public:
+        struct bucket
+        {
+            unsigned int infobyte;
+            Type space[32];
+        };
+        bucket *table;
+        unsigned int table_size;
+
     private:
-        auto hash(const char *) -> unsigned int;
+        auto hash(const unsigned char *) -> unsigned int;
+        inline auto resize(void) -> void;
 
     public:
-        Namespace();
+        Namespace(void);
     };
 }
 
