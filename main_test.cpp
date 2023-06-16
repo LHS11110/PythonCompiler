889b2e1b975b7cfe22b9b9a296d438908a9b7807
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-16 16:01:45 +0900

    Update main_test.cpp

--- main_test.cpp
+++ main_test.cpp
@@ -1,11 +1,18 @@
 #include "Library/Namespace.hpp"
 #include <iostream>
+#include <time.h>
+#include <unordered_map>
 using namespace std;
 
 auto main(void) -> int
 {
-    cout << sizeof(pyc::Namespace<short>::bucket::space) << '\n';
-    cout << (8 * 32) << '\n';
+    unordered_map<int, int> m;
+
+    clock_t s = clock();
+    for (long long i = 0; i < 500000000; i++)
+    {
+    }
+    cout << float(clock() - s) / CLOCKS_PER_SEC << '\n';
 
     return 0;
 }
