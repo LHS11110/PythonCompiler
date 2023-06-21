6274f04f3b608762678416ba26511a3510cace19
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-22 03:14:05 +0900

    Update Identifier.hpp

--- Library/Identifier.hpp
+++ Library/Identifier.hpp
@@ -1,6 +1,7 @@
 #ifndef _IDENTIFIER_HPP
 #define _IDENTIFIER_HPP
 #include <cstring>
+#include <cstdlib>
 
 class identifier
 {
@@ -12,7 +13,8 @@ public:
     identifier();
     identifier(const char *_str);
 
-    auto hash() -> unsigned int;
+    auto hash() const -> unsigned int;
+    auto operator==(const identifier &) const -> bool;
 };
 
 #endif
