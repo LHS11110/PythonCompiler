9aeb82480c0eafe4927117cf430c4b4943a60ed1
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-23 21:29:28 +0900

    Update main_test.cpp

--- main_test.cpp
+++ main_test.cpp
@@ -7,14 +7,6 @@ using namespace std;
 
 auto main(void) -> int
 {
-    pyc::Namespace<identifier, int> m;
-
-    clock_t s = clock();
-    string str;
-    for (int i = 0; i < 10000000; i++)
-        m["asd"] = i;
-    cout << float(clock() - s) / CLOCKS_PER_SEC << '\n';
-    cout << *m.find("asd") << '\n';
 
     return 0;
 }
