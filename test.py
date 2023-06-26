68ed46333624000af7c5789c121fe94ff13a3998
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-27 01:38:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-from Modules.Parser import Parser
-from Modules.Lexer import Lexer
-from Modules.Parser import Container
-from Modules.Parser import Object
-from Modules.Parser import Expression
-from Modules.Parser import Checker
-from Modules.Parser import Syntax
-from Modules.Parser.Expression import priority
-import time
+import os
 
-d: dict[int, int] = {}
-s: float = time.time()
-for i in range(10000000):
-    d[i] = i
-print(time.time() - s)
+print(os.path.dirname(os.path.abspath(__file__)), __file__)
