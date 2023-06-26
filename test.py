730a367ea4fb389a7f9c89040fe3a7ab47907ffa
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-27 03:23:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Modules.Parser.Expression import priority
 from Modules.Parser import Parser
-from Modules.Lexer import Lexer
+from Modules.LexicalAnalyzer import LexicalAnalyze
 from Modules.Parser import Container
 from Modules.Parser import Object
 from Modules.Parser import Expression
@@ -12,19 +12,21 @@ input_txt: str = "a.a[1]() for i, j, asd123 in range(10)\n"
 
 print(input_txt)
 print(
-    Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)),
+    Parser.cleanup(LexicalAnalyze.lexical_analyze(input_text=input_txt)),
     end="\n\n",
 )
 
 print(
-    Syntax.getGenerator(Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)), 0),
+    Syntax.getGenerator(
+        Parser.cleanup(LexicalAnalyze.lexical_analyze(input_text=input_txt)), 0
+    ),
     end="\n\n",
 )
 
 print(
-    Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt))[
+    Parser.cleanup(LexicalAnalyze.lexical_analyze(input_text=input_txt))[
         Syntax.getGenerator(
-            Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)),
+            Parser.cleanup(LexicalAnalyze.lexical_analyze(input_text=input_txt)),
             0,
         )[1] :
     ],
