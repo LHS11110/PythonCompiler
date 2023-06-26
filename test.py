afcf12d942ab4017d968e01eedd74640ca85a3a2
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-27 02:26:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,32 @@
-import os
+from Modules.Parser.Expression import priority
+from Modules.Parser import Parser
+from Modules.Lexer import Lexer
+from Modules.Parser import Container
+from Modules.Parser import Object
+from Modules.Parser import Expression
+from Modules.Parser import Checker
+from Modules.Parser import Syntax
+from Modules.Parser.Expression import priority
 
-print(os.path.dirname(os.path.abspath(__file__)), __file__)
+input_txt: str = "a.a[1]() for i, j, asd123 in range(10)\n"
+
+print(input_txt)
+print(
+    Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)),
+    end="\n\n",
+)
+
+print(
+    Syntax.getGenerator(Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)), 0),
+    end="\n\n",
+)
+
+print(
+    Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt))[
+        Syntax.getGenerator(
+            Parser.cleanup(Lexer.lexical_analyze(input_text=input_txt)),
+            0,
+        )[1] :
+    ],
+    end="\n\n",
+)
