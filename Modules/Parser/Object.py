f0356c61b50adbf18b08c4b42e823b40e315923b
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-27 02:22:08 +0900

    Update Object.py

--- Modules/Parser/Object.py
+++ Modules/Parser/Object.py
@@ -1,21 +1,21 @@
 from typing import Any, Callable
 
 
-def getVar(codes: list[tuple[str, str]], idx: int) -> tuple[dict[str, Any], int]:
-    assert codes[idx][0] == "IDENTIFIER", ""
+def getVar(tokens: list[tuple[str, str]], idx: int) -> tuple[dict[str, Any], int]:
+    assert tokens[idx][0] == "IDENTIFIER", ""
     tree: dict[str, Any] = {}
     tree["Category"] = "Object"
     tree["ObjectType"] = "Var"
-    tree["Name"] = codes[idx][1]
+    tree["Name"] = tokens[idx][1]
     return (tree, idx + 1)
 
 
-def getLiteral(codes: list[tuple[str, str]], idx: int) -> tuple[dict[str, Any], int]:
-    assert codes[idx][0] in ["NUMBER", "STRING", "BOOL"], ""
+def getLiteral(tokens: list[tuple[str, str]], idx: int) -> tuple[dict[str, Any], int]:
+    assert tokens[idx][0] in ["NUMBER", "STRING", "BOOL"], ""
     tree: dict[str, Any] = {}
     tree["Category"] = "Object"
-    tree["ObjectType"] = codes[idx][0]
-    tree["Value"] = codes[idx][1]
+    tree["ObjectType"] = tokens[idx][0]
+    tree["Value"] = tokens[idx][1]
     return (tree, idx + 1)
 
 
