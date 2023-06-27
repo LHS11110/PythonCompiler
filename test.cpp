09758bbeab2f1d632087b3be4eb64c02e45c228c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-06-28 02:33:55 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -37,21 +37,7 @@ public:
 
 auto main(void) -> int
 {
-    pyc::set<integer, int> *m = new pyc::set<integer, int>();
-    int e;
-    cin >> e;
-    clock_t s = clock();
-    for (int i = 0; i < e; i++)
-        (*m)[i] = i;
-    std::cout << ((float)(clock() - s) / CLOCKS_PER_SEC) << '\n';
-    std::cout << m->size() << '\n';
-    cout << "\n\n";
-
-    s = clock();
-    for (int i = 0; i < e; i++)
-        m->remove(i);
-    std::cout << ((float)(clock() - s) / CLOCKS_PER_SEC) << '\n';
-    cout << m->operator[](213) << '\n';
+    pyc::set<integer, int> &m = *new pyc::set<integer, int>();
 
     return 0;
 }
